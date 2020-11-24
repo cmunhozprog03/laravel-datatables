ddd6ecb4f8cf19d403e70ad688edc6a12bc1171d
cmunhozprog03 <cmunhozprog03@hotmail.com>
2020-11-24 05:39:00 -0300

    create Project

---
+++ database/migrations/2020_11_23_185808_create_categorias_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nome')->unique();
+            $table->text('descricao');
+            $table->string('tipo');
+            $table->string('ativo');
+            $table->text('imagem');
+            $table->integer('ordem');
+            $table->string('slug');
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+}
